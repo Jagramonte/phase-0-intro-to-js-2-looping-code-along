d26a3d5c792e7a1a988fcf4155f3fec312fd8584
Joseph Agramonte <lifeinabundance314@gmail.com>
2023-08-14 21:05:19 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const names = (["Guadalupe", "Ollie", "Aki"])
+function writeCards(names) {
+    let array = [];
+    for( let i=0; i<names.length; i += 1) {
+        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}
+return array;
+}
+function countDown() {
+    let n = 10;
+    while (n>=0) {
+        console.log(n --);
+    }
+}
